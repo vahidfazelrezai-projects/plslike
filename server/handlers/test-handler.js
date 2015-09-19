d4fa44ea3f5b66bacf11720760f6133359b31370
Vahid Fazel-Rezai <vahidfazelrezai@gmail.com>
2015-09-19 14:55:46 -0400

    add vendors

--- server/handlers/test-handler.js
+++ server/handlers/test-handler.js
@@ -1,4 +1,9 @@
 var testHandler = {};
+var Parse = require('parse').Parse;
+var Clarifai = require('./clarifai_node.js');
+var ig = require('instagram-node').instagram();
+Parse.initialize(JbKjS5vCcu4rzuQAEJlXafeBJuA3rhMXq82xiKyN, PnyvKOrEmngMeKRSKlKNiBxkw0lHeYySp0sgTckJ);
+
 
 testHandler.test = function (req, res) {
 
