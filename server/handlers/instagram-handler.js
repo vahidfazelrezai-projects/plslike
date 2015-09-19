639ce002a5fcf38899da262cbe00f906db6cb1f7
Vahid Fazel-Rezai <vahidfazelrezai@gmail.com>
2015-09-19 16:20:35 -0400

    comment stuff out for heroku

--- server/handlers/instagram-handler.js
+++ server/handlers/instagram-handler.js
@@ -25,47 +25,47 @@ instagramHandler.callback = function(req, res) {
     }
   });
 };
-
-
-instagramHandler.loginUser = function(access, client, secret) {
-    ig.use({ access_token: access });
-}
-
-instagramHandler.isPublic = function() {
-}
-
-instagramHandler.getFollowers = function() {
-    return ig.user_followers('self', function(err, users, pagination, remaining, limit) {});
-}
-
-instagramHandler.getFollowees = function(user_id) {
-    return ig.user_follows(user_id, function(err, users, pagination, remaining, limit) {});
-}
-
-instagramHandler.getPictureIDs = function(user_id) {
-    return ig.user_media_recent(user_id, [100] function(err, medias, pagination, remaining, limit) {});
-
-}
-
-instagramHandler.getPicture = function(media_id) {
-    return ig.media(media_id, function(err, media, remaining, limit) {});
-}
-
-instagramHandler.checkLike = function(user_id, media_id) {
-    return user_id in ig.likes(media_id, function(err, result, remaining, limit) {});
-}
-
-instagramHandler.getPeers = function(user_id) {
-    var followers = instagramHandler.getFollowers(user_id);
-    var peers = [];
-    for (i = 0; i < followers.length; i++) {
-        follower = followers[i];
-        followees = instagramHandler.getFollowees(follower);
-        for (j = 0; j < followees.length; j++) {
-            peers.push(followees[j]);
-        }
-    }
-    // remove duplicates from peers ??
-}
+//
+//
+// instagramHandler.loginUser = function(access, client, secret) {
+//     ig.use({ access_token: access });
+// }
+//
+// instagramHandler.isPublic = function() {
+// }
+//
+// instagramHandler.getFollowers = function() {
+//     return ig.user_followers('self', function(err, users, pagination, remaining, limit) {});
+// }
+//
+// instagramHandler.getFollowees = function(user_id) {
+//     return ig.user_follows(user_id, function(err, users, pagination, remaining, limit) {});
+// }
+//
+// instagramHandler.getPictureIDs = function(user_id) {
+//     return ig.user_media_recent(user_id, [100] function(err, medias, pagination, remaining, limit) {});
+//
+// }
+//
+// instagramHandler.getPicture = function(media_id) {
+//     return ig.media(media_id, function(err, media, remaining, limit) {});
+// }
+//
+// instagramHandler.checkLike = function(user_id, media_id) {
+//     return user_id in ig.likes(media_id, function(err, result, remaining, limit) {});
+// }
+//
+// instagramHandler.getPeers = function(user_id) {
+//     var followers = instagramHandler.getFollowers(user_id);
+//     var peers = [];
+//     for (i = 0; i < followers.length; i++) {
+//         follower = followers[i];
+//         followees = instagramHandler.getFollowees(follower);
+//         for (j = 0; j < followees.length; j++) {
+//             peers.push(followees[j]);
+//         }
+//     }
+//     // remove duplicates from peers ??
+// }
 
 module.exports = instagramHandler;
