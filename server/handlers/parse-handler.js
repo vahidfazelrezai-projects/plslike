a2865c7ef67806e89d12c556fcb14037f76a2f1b
mefeakengin <akengin@mit.edu>
2015-09-19 21:08:51 -0400

    pls like me scotttt

--- server/handlers/parse-handler.js
+++ server/handlers/parse-handler.js
@@ -1,9 +1,17 @@
-var Parse = require('parse').Parse;
+var Parse = require('parse/node').Parse;
 var config = require('../config/config');
+var $ = require('jquery');
+
+// usage
 
 var parseHandler = {};
 
 Parse.initialize(config.parse.applicationId, config.parse.javascriptKey);
+
+var TestObject = Parse.Object.extend("TestObject");
+var testObject = new TestObject();
+testObject.save({foo: "bar"}).then(function(object) {
+});
 
 parseHandler.test = function (req, res) {
     res.send('parse!');
