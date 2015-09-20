3d2b08eb9b457f824a7095d9a066069c80ee3220
Vahid Fazel-Rezai <vahidfazelrezai@gmail.com>
2015-09-19 20:43:06 -0400

    well it did not work

--- server/handlers/instagram-handler.js
+++ server/handlers/instagram-handler.js
@@ -75,40 +75,31 @@ instagramHandler.getMedias = function (user_id, callback) {
 
 instagramHandler.test = function (req, res) {
     var user_id = 'self';
+    var response = {};
     var pos_urls = [];
     var neg_urls = [];
     instagramHandler.getFollows(user_id, function (follows) {
         for (var i = 0; i < follows.length; i++) {
             instagramHandler.getMedias(follows[i], function (medias) {
-                if (medias) {
-                    for (var j = 0; j < medias.length; j++) {
-                        var likers = medias[j]['likes']['data'];
-                        var url = medias[j]['images']['standard_resolution']['url']
-                        for (var k = 0; k < likers.length; k++) {
-                            if (likers[k] == user_id) {
-                                pos_urls.push(url);
-                            } else {
-                                neg_urls.push(url);
-                            }
+                for (var j = 0; j < medias.length; j++) {
+                    var likers = medias[j]['likes']['data'];
+                    var url = medias[j]['images']['standard_resolution']['url'];
+                    response.likers = likers;
+                    response.url = url;
+                    for (var k = 0; k < likers.length; k++) {
+                        if (likers[k] == user_id) {
+                            pos_urls.push(url);
+                        } else {
+                            neg_urls.push(url);
                         }
                     }
                 }
             });
         }
-        res.send(JSON.stringify({
-            'negative': neg_urls,
-            'positive': pos_urls
-        }));
+        response.negative = neg_urls;
+        response.positive = pos_urls;
+        res.send(JSON.stringify(response));
     });
-    // instagramHandler.getFollowers('self', function(followers) {
-    //     for (i = 0; i < followers.length; i++) {
-    //         follower = followers[i];
-    //         console.log(follower);
-    //         instagramHandler.getPictures(follower, function(media) {
-    //             res.send(media);
-    //         })
-    //     }
-    // });
 }
 
 module.exports = instagramHandler;
